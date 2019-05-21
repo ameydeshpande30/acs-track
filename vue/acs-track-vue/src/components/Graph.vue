0bebcc75ab880f273c9d89903ca7e12f21b0d725
1317Stephen <1317stephen@gmail.com>
2019-05-21 18:45:21 +0900

    [0521] Vue.js make new project

---
+++ vue/acs-track-vue/src/components/Graph.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="graph-body">
+    <h1>Graph</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Graph',
+  data () {
+    return {
+      msg: 'Graph Page'
+    }
+  }
+}
+</script>
